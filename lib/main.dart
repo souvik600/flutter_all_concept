05cb8cdc1aa91b498b5098ef8f8628a7c2699472
souvik600 <souvikdas0600das@gmail.com>
2023-09-26 13:50:05 +0600

    StateFullWidget_0f_flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,74 @@
 import 'package:flutter/material.dart';
 
-///StatelessWidget
+/// StateFullWidget_in_Flutter
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+  void _decrementCounter(){
+    setState(() {
+      _counter--;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("StatelessWidget"),
+          title: Text("StateFullWidget"),
         ),
-        body: HomePage(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Pressed Button : $_counter",
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 24),
+              ),
+              SizedBox(height: 30,),
+              Text(
+                "$_counter",
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.purple),
+              ),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(onPressed: _incrementCounter, child:Icon(Icons.add)),
+                    SizedBox(width: 15,),
+                    ElevatedButton(onPressed: _decrementCounter, child:Icon(Icons.minimize))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: _incrementCounter,
+        //   tooltip: " Press Button",
+        //   child: Icon(Icons.add),
+        // ),
       ),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      child: Center(
-          child: Text(
-        "hello flutter",
-        style: TextStyle(
-            fontSize: 24, color: Colors.white, fontWeight: FontWeight.w500),
-      )),
     );
   }
 }
