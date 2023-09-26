7d65d50fee02b85fb79501fd38a05fe30810c2b6
souvik600 <souvikdas0600das@gmail.com>
2023-09-26 21:09:27 +0600

    Container_0f_flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,51 @@
 import 'package:flutter/material.dart';
 
-/// StateFullWidget_in_Flutter
+/// Container_in_Flutter
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-  void _decrementCounter(){
-    setState(() {
-      _counter--;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("StateFullWidget"),
+          title: Text("Container"),
         ),
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                "Pressed Button : $_counter",
-                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 24),
-              ),
-              SizedBox(height: 30,),
-              Text(
-                "$_counter",
-                style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.w800,
-                    color: Colors.purple),
-              ),
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(onPressed: _incrementCounter, child:Icon(Icons.add)),
-                    SizedBox(width: 15,),
-                    ElevatedButton(onPressed: _decrementCounter, child:Icon(Icons.minimize))
-                  ],
+              Container(
+                width: 500,
+                height: 100,
+                margin: EdgeInsets.all(
+                  10,
                 ),
+                padding: EdgeInsets.all(10),
+                color: Colors.purpleAccent,
+                child: Text(
+                  "This is Container",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      backgroundColor: Colors.green),
+                ),
+
+              ),
+              Container(
+                width: 500,
+                height: 100,
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(10),
+                color: Colors.green,
               )
             ],
           ),
         ),
-        // floatingActionButton: FloatingActionButton(
-        //   onPressed: _incrementCounter,
-        //   tooltip: " Press Button",
-        //   child: Icon(Icons.add),
-        // ),
       ),
     );
   }
