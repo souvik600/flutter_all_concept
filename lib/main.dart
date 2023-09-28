a4c0e134857994c5c80d946067e87f9e5c3452e3
souvik600 <souvikdas0600das@gmail.com>
2023-09-28 12:30:01 +0600

    Container_0f_flutter

--- lib/main.dart
+++ lib/main.dart
@@ -16,32 +16,55 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Container"),
         ),
-        body: Center(
+        body: SingleChildScrollView(
           child: Column(
             children: [
-              Container(
-                width: 500,
-                height: 100,
-                margin: EdgeInsets.all(
-                  10,
+              Center(
+                child: Container(
+                  width: 500,
+                  height: 100,
+                  color: Colors.grey,
+                  margin: EdgeInsets.all(20),
+                  padding: EdgeInsets.all(10),
+                  child: Center(
+                    child: Text(
+                      "This is 1st Container",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
                 ),
-                padding: EdgeInsets.all(10),
-                color: Colors.purpleAccent,
-                child: Text(
-                  "This is Container",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      backgroundColor: Colors.green),
-                ),
-
               ),
-              Container(
-                width: 500,
-                height: 100,
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.all(10),
-                color: Colors.green,
+              Center(
+                child: Container(
+                  width: 500,
+                  height: 100,
+                  //color: Colors.lime,
+                  margin: EdgeInsets.all(20),
+                  padding: EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.lightGreen,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.lightBlue,
+                            blurRadius: 5,
+                            offset: Offset(2, 2))
+                      ]),
+                  constraints: BoxConstraints(
+                    maxHeight: 300,
+                    maxWidth: 800,
+                  ),
+                  //baka korar jonno
+                  transform: Matrix4.rotationZ(0.1),
+                  child: Text(
+                    " This is container 2 ",
+                    style: TextStyle(
+                        color: Colors.pink,
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
               )
             ],
           ),
