f5727bc0c21b3a55ed4e6739223df3f6c8d872de
souvik600 <souvikdas0600das@gmail.com>
2023-10-02 00:13:06 +0600

    Button_0f_flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
+// Button In Flutter
 
+import 'package:flutter/material.dart';
+const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
+enum SampleItem { itemOne, itemTwo, itemThree }
 void main() {
   runApp(MyApp());
 }
@@ -10,49 +13,104 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  String dropdownValue = list.first;
+  SampleItem? selectedMenu;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Image in flutter"),
+          title: const Text("Button"),
         ),
-        body: SingleChildScrollView(
-          child: Column( mainAxisAlignment: MainAxisAlignment.center,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Center(
-                child: Column(
-                  children: [
-                    Container(
-                      width: 400,
-                      height: 250,
-                      color: Colors.tealAccent,
-                      child: Image.asset('assets/souvik.jpg',),
-                    ),
-                    Text('This is Assets image',style: TextStyle(fontWeight: FontWeight.w500,fontSize: 18),)
-                  ],
-                ),
+              OutlinedButton(
+                  onPressed: () {
+                    print("Outline button pressed");
+                  },
+                  child: const Text("OutLinedButton")),
+              ElevatedButton(
+                  onPressed: () {
+                    print("pressed elevated button");
+                  },
+                  child: const Text("Elevated Button")),
+              TextButton(
+                  onPressed: () {
+                    print("TextButton pressed");
+                  },
+                  child: const Text("Text Button")),
+              IconButton(
+                  onPressed: () {
+                    print('Icon Button');
+                  },
+                  icon: const Icon(Icons.add)),
+              FloatingActionButton(
+                onPressed: () {
+                  print('Floating action Button');
+                },
+                child: Icon(Icons.add_alarm_rounded),
               ),
-              SizedBox(height: 20),
-              Column(
-                children: [
-                  Container(
-                    width: 400,
-                    height: 250,
-                    color: Colors.yellowAccent,
-                    child: Image.network('https://cdn.pixabay.com/photo/2018/02/15/21/55/sunset-3156440_1280.jpg'),
+              DropdownButton(
+                value: dropdownValue,
+                icon: const Icon(Icons.arrow_downward),
+                elevation: 16,
+                style: const TextStyle(color: Colors.deepPurple),
+                underline: Container(
+                  height: 2,
+                  color: Colors.deepPurpleAccent,
+                ),
+                onChanged: (String? value) {
+                  // This is called when the user selects an item.
+                  setState(() {
+                    dropdownValue = value!;
+                  });
+                },
+                items: list.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              PopupMenuButton<SampleItem>(
+                initialValue: selectedMenu,
+                // Callback that sets the selected popup menu item.
+                onSelected: (SampleItem item) {
+                  setState(() {
+                    selectedMenu = item;
+                  });
+                },
+                itemBuilder: (BuildContext context) => <PopupMenuEntry<SampleItem>>[
+                  const PopupMenuItem<SampleItem>(
+                    value: SampleItem.itemOne,
+                    child: Text('Item 1'),
                   ),
-                  Text("This is network Image",style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500),),
-                  Container(
-                    width: 400,
-                    height: 250,
-                    color: Colors.brown,
-                    child: Center(
-                      child: TextButton(onPressed: () {  }, child: Icon(Icons.add_a_photo,size: 50,),
-
-                      ),
-                    ),
-
-                  )
+                  const PopupMenuItem<SampleItem>(
+                    value: SampleItem.itemTwo,
+                    child: Text('Item 2'),
+                  ),
+                  const PopupMenuItem<SampleItem>(
+                    value: SampleItem.itemThree,
+                    child: Text('Item 3'),
+                  ),
                 ],
-              )
+              ),
             ],
           ),
         ),
